ddb77f5d060ff50cfa23de970ef851043dffeea4
Abhishek Anand <abhishek.anand@sourcebits.com>
2012-06-28 19:55:32 +0530

    changing constant name

--- lib/neospeech/string.rb
+++ lib/neospeech/string.rb
@@ -1,7 +1,7 @@
 module Neospeech
   module String
     def speak voice, speed = 'normal'
-      conversion = TextToSpeech::Neospeech::Convertor.new text: self, voice: voice, speed: speed
+      conversion = Neospeech::Convertor.new text: self, voice: voice, speed: speed
       conversion.queue
       sleep(1) until conversion.finished?
       conversion.audio_file_url
